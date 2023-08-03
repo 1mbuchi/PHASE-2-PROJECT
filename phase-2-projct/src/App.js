8837b55ee0d72c8ffd3a89d653bf96a596d349b4
1mbuchi Natasha <natashaimbuchi@gmail.com>
2023-08-03 11:07:12 +0300

    fetching meal data from the api url

--- phase-2-projct/src/App.js
+++ phase-2-projct/src/App.js
@@ -8,3 +8,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+
+const API_URL = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+
+
