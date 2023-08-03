421c61b9bb2c7be33bb2603f351f1fdfc641a9f0
1mbuchi Natasha <natashaimbuchi@gmail.com>
2023-08-03 11:34:56 +0300

    contact bar

---
+++ phase-2-projct/src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>
+        If you have any questions or feedback, please feel free to contact us.
+        <p>natashaimbuchi@gmail.com</p>
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
