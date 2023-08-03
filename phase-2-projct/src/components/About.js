3cee70ad16cd0267b9120637890fa787ca4a134d
1mbuchi Natasha <natashaimbuchi@gmail.com>
2023-08-03 11:35:58 +0300

    aboutbar

---
+++ phase-2-projct/src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+     <h1>About Us</h1>
+      <p>
+        We are a restaurant that takes pride in serving delicious meals to our customers. Our
+        passion for food and excellent customer service sets us apart from others.
+      </p>
+    </div>
+  );
+};
+
+export default About;
