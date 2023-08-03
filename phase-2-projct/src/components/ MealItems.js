bda99589a8e70cd3cf3ac6af0ad89d4ccd08802b
1mbuchi Natasha <natashaimbuchi@gmail.com>
2023-08-03 11:25:55 +0300

    mealitems components rendered

--- phase-2-projct/src/components/ MealItems.js
+++ phase-2-projct/src/components/ MealItems.js
@@ -2,3 +2,20 @@ import React from "react";
 
 const MealItem = ({ meal }) => {
   const { strMeal, strCategory, strArea, strInstructions, strMealThumb, strYoutube } = meal;
+  return (
+    <li className="meal-item">
+      <img className="meal-image" src={strMealThumb} alt={strMeal} />
+      <div className="meal-details">
+        <h2 className="meal-name">{strMeal}</h2>
+        <p className="meal-category">Category: {strCategory}</p>
+        <p className="meal-area">Area: {strArea}</p>
+        <p className="meal-instructions">Instructions: {strInstructions}</p>
+        <a className="meal-youtube" href={strYoutube} target="_blank" rel="noopener noreferrer">
+          Watch on YouTube
+        </a>
+      </div>
+    </li>
+  );
+};
+
+export default MealItem;
