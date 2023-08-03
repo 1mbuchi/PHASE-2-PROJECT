70506597c1748a38d06f2e7a59b3388fa50971fc
1mbuchi Natasha <natashaimbuchi@gmail.com>
2023-08-03 11:14:39 +0300

    initial data fetching

--- phase-2-projct/src/App.js
+++ phase-2-projct/src/App.js
@@ -11,9 +11,22 @@ import Contact from './components/Contact';
 
 const API_URL = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
-function App() 
+function App() {
   const [meals, setMeals] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  
+  useEffect(() => {
+    fetchMeals();
+  }, []);
+
+  const fetchMeals = async () => {
+    try {
+      const response = await axios.get(API_URL);
+      setMeals(response.data.meals);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching meals:", error);
+      setLoading(false);
+    }
+  };
