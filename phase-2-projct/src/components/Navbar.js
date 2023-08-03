c6a1d6b85129e92539336f4a7b1d6e0d03498c53
1mbuchi Natasha <natashaimbuchi@gmail.com>
2023-08-03 11:29:52 +0300

    handle navigation

---
+++ phase-2-projct/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <Link className="navbar-brand" to="/">Yummy Restaurant</Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
